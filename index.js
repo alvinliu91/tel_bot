a4a1893fa60632b431c07283211e93a5d494787a
alvinliu <alvinyeetsun@gmail.com>
2017-04-21 15:53:22 +0800

    bot

---
+++ index.js
@@ -0,0 +1,60 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+const axios = require('axios')
+
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({
+  extended: true
+})); 
+
+
+app.post('/new-message', function(req, res) {
+  const {message} = req.body
+
+  if (!message) {
+    return res.end()
+  }
+
+  if (message.text[0] == '/')
+    message.text = message.text.substr(6)
+  else
+    message.text = message.text.substr(0)
+
+  var search = require('youtube-search');
+ 
+  var opts = {
+    maxResults: 10,
+    key: 'AIzaSyBEM0aTfvlVm0gHZa2IZKKbwGIwCEfqRxw'
+  };
+ 
+  search(message.text , opts, function(err, results) {
+    if(err) return console.log(err);
+ 
+    console.dir(results);
+  });
+
+
+
+  axios.post('https://api.telegram.org/bot374707652:AAE2kJrfHiS3zQtnmHhxglx2hIpYJb85TsQ/sendMessage', {
+    chat_id: message.chat.id,
+    //text: 'https://www.youtube.com/results?search_query='+encodeURIComponent(message.text)
+    text: results
+  })
+    .then(response => {
+      console.log('Message posted')
+      res.end('ok')
+    })
+    .catch(err => {
+      console.log('Error :', err)
+      res.end('Error :' + err)
+    })
+
+});
+
+
+app.listen(3000, function() {
+  console.log('Telegram app listening on port 3000!');
+});
+
+
