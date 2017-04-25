dcaf8bc11c44c68a51e8d6ddab0dfe08e2ca3fdd
alvinliu <alvinyeetsun@gmail.com>
2017-04-25 10:44:52 +0800

    bot

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.post('/new-message', function(req, res) {
 
   axios.post('https://api.telegram.org/bot374707652:AAE2kJrfHiS3zQtnmHhxglx2hIpYJb85TsQ/sendMessage', {
     chat_id: message.chat.id,
-    text: 'https://www.youtube.com/results?search_query='+encodeURIComponent(message.text)
+    //text: 'https://www.youtube.com/results?search_query='+encodeURIComponent(message.text)
     
 //======================================================
   search(message.text , opts, function(err, results) {
