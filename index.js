08b4c7ef05b100867e92c6c38e8847ad17e16618
alvinliu <alvinyeetsun@gmail.com>
2017-04-25 10:36:01 +0800

    bot

--- index.js
+++ index.js
@@ -21,12 +21,12 @@ app.post('/new-message', function(req, res) {
   else
     message.text = message.text.substr(0)
 //======================================================
-//  var search = require('youtube-search');
+  var search = require('youtube-search');
  
-//  var opts = {
-//    maxResults: 10,
-//    key: 'AIzaSyBEM0aTfvlVm0gHZa2IZKKbwGIwCEfqRxw'
-//  };
+  var opts = {
+    maxResults: 10,
+    key: 'AIzaSyBEM0aTfvlVm0gHZa2IZKKbwGIwCEfqRxw'
+  };
  
 //=====================================================
 
